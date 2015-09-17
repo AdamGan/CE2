d18f9ff2bd981be780dee364c3e646b246989ddf
Adam <adamgan0527@gmail.com>
2015-09-17 14:46:25 +0800

    Updated Group ID Replaced global arraylist with local Updated methods to return values instead of void to do JUnit testing

--- TextBuddy.java
+++ TextBuddy.java
@@ -1,6 +1,6 @@
 /* Name: Gan Wen Jie Adam
  * Student No: A0125495Y
- * Group: W10-2J
+ * Group: W10-4J
  * Submission: CE1
  */
 
@@ -9,137 +9,169 @@ import java.util.*;
 
 public class TextBuddy {
 
-    private static final String MSG_NO_FILE_SPECIFIED = "No filename specified";
-    private static final String MSG_WELCOME = "Welcome to TextBuddy. %1$s is ready for use";
-    private static final String MSG_PROMPT = "command: ";
-    private static final String MSG_INVALID_COMMAND = "invalid command";
-    private static final String MSG_INVALID_INDEX = "Invalid index";
-    private static final String MSG_DELETED = "deleted from %1$s: \"%2$s\"";
-    private static final String MSG_ADDED = "added to %1$s: \"%2$s\"";
-    private static final String MSG_CLEARED = "all content deleted from %1$s";
-    private static final String MSG_EMPTY = "%!$s is empty";
-    private static final String MSG_DISPLAY = "%1$s. %2$s";
-    
-    // global variables and objects
-    private static PrintWriter writer = null;
-    private static ArrayList<String> text = new ArrayList<>();
-    private static String fileName = "";
+	private static final String MSG_NO_FILE_SPECIFIED = "No filename specified";
+	private static final String MSG_WELCOME = "Welcome to TextBuddy. %1$s is ready for use";
+	private static final String MSG_PROMPT = "command: ";
+	private static final String MSG_INVALID_COMMAND = "invalid command";
+	private static final String MSG_INVALID_INDEX = "Invalid index";
+	private static final String MSG_DELETED = "deleted from %1$s: \"%2$s\"";
+	private static final String MSG_ADDED = "added to %1$s: \"%2$s\"";
+	private static final String MSG_CLEARED = "all content deleted from %1$s";
+	private static final String MSG_EMPTY = "%1$s is empty";
+	private static final String MSG_DISPLAY = "%1$s. %2$s";
+	
+	// global variables and objects
+	private static PrintWriter writer = null;
+	private ArrayList<String> text = new ArrayList<String>();
+	private static String fileName = "";
+	Scanner reader = new Scanner(System.in);
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
+		TextBuddy myTextBuddy = new TextBuddy();
+		
+		myTextBuddy.run(args);
+	}
+	
+	//start of program, creation of file and reading of input
+	void run(String[] args) {
+		if(args.length==0) {
+			showToUser(MSG_NO_FILE_SPECIFIED);
+			System.exit(1);
+		}
+		else {
+			Scanner reader = new Scanner(System.in);
+			createFile(args);
 
-        if(args.length==0) {
-            showToUser(MSG_NO_FILE_SPECIFIED);
-            System.exit(1);
-        }
-        else {
-            fileName = args[0];
-            Scanner reader = new Scanner(System.in);
-            try {
-                writer = new PrintWriter(fileName);
-            } 
-            catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
+			//welcome msg and continue to prompt for input
+			showToUser(String.format(MSG_WELCOME, fileName));
+			promptInput(writer, reader);
+		}
+	}
+	
+	//creates file based on name passed in args[0]
+	//returns true if file creation is successful and false if not
+	static boolean createFile(String[] args) {
+		boolean success = false;
+		fileName = args[0];
+		
+		try {
+			writer = new PrintWriter(fileName);
+			success = true;
+		} 
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		return success;
+	}
+	
+	//simple printer
+	void showToUser(String text) {
+		System.out.println(text);
+	}
+	
+	//recursive method to prompt for user input until exit
+	void promptInput(PrintWriter writer, Scanner reader) {
+		System.out.print(MSG_PROMPT);
 
-            //welcome msg and continue to prompt for input
-            showToUser(String.format(MSG_WELCOME, fileName));
-            prompt(writer, reader);
-        }
-    }
+		String input = reader.nextLine();
+		String[] temp = input.split("\\s",2);
 
-    private static void showToUser(String text) {
-        System.out.println(text);
-    }
-    
-    //recursive method to prompt for user input until exit
-    private static void prompt(PrintWriter writer, Scanner reader) {
-        System.out.print(MSG_PROMPT);
-
-        String input = reader.nextLine();
-        String[] temp = input.split("\\s",2);
-
-        //switch based on user command
-        switch (temp[0]) {
-        case "add": addText(temp[1]);
-        break;
-        case "display": display();
-        break;
-        case "delete":  int index = Integer.parseInt(temp[1]);
-                        deleteText(index);
-        break;
-        case "clear": clear();
-        break;
-        case "exit": exit();
-        break;
-        default: invalid(reader);
-        }
-        prompt(writer, reader); //recursive prompt next command 
-    }
-
-
-    //unrecognized command
-    private static void invalid(Scanner reader) {
-        // TODO Auto-generated method stub
-        showToUser(MSG_INVALID_COMMAND);
-        prompt(writer, reader);
-    }
-
-    private static void exit() {
-        // TODO Auto-generated method stub
-
-        if(text.size() != 0) {
-            for(int i = 0; i < text.size(); i++) {
-                writer.println(text.get(i));
-            }
-            writer.close();
-        }
-        System.exit(1);
-    }
-
-
-    public static void clear() {
-        // TODO Auto-generated method stub
-        try {
-            writer = new PrintWriter(fileName);
-            showToUser(String.format(MSG_CLEARED, fileName));
-        } 
-        catch (FileNotFoundException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        text.clear();
-    }
-
-    //delete
-    private static void deleteText(int index) {
-        // TODO Auto-generated method stub
-
-        if(index<1 || index-1>text.size()) {
-            showToUser(MSG_INVALID_INDEX);
-        }
-        else {
-            String tempRemoved = text.get(index-1);
-            text.remove(index-1);
-            showToUser(String.format(MSG_DELETED, fileName, tempRemoved));
-        }
-    }
-
-    private static void display() {
-        // TODO Auto-generated method stub
-        if(text.size() == 0)
-            showToUser(String.format(MSG_EMPTY, fileName));
-        else {
-            for(int i = 0; i < text.size(); i++) {
-                showToUser(String.format(MSG_DISPLAY, i+1, text.get(i)));
-            }
-        }
-    }
-
-    private static void addText(String temp) {
-        // TODO Auto-generated method stub
-        text.add(temp);
-        showToUser(String.format(MSG_ADDED, fileName, temp));
-    }
+		//switch based on user command
+		switch (temp[0]) {
+		case "add": 	text = addText(text, temp[1]);
+						break;
+						
+		case "display": display(text);
+						break;
+						
+		case "delete":	int index = Integer.parseInt(temp[1]);
+						text = deleteText(text,index);
+						break;
+						
+		case "clear": 	text = clear(text);
+						break;
+						
+		case "exit": 	exit();
+						break;
+						
+		default: 		invalid(reader);
+		}
+		//recursive prompt next user input
+		promptInput(writer, reader); 
+	}
 
 
+	//input is an unrecognized command
+	//display error and prompt for input again
+	void invalid(Scanner reader) {
+		showToUser(MSG_INVALID_COMMAND);
+		promptInput(writer, reader);
+	}
+	
+	
+	void exit() {
+
+		if(text.size() != 0) {
+			for(int i = 0; i < text.size(); i++) {
+				writer.println(text.get(i));
+			}
+			writer.close();
+		}
+		System.exit(1);
+	}
+
+	//clear all existing inputs and return the arraylist
+	public ArrayList<String> clear(ArrayList<String> myText) {
+		
+		try {
+			writer = new PrintWriter(fileName);
+			showToUser(String.format(MSG_CLEARED, fileName));
+		} 
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		myText.clear();
+		
+		return myText;
+	}
+
+	//delete based on line index from user and returns new arraylist
+	public ArrayList<String> deleteText(ArrayList<String> myText, int index) {
+		
+		if(index<1 || index-1>text.size()) {
+			showToUser(MSG_INVALID_INDEX);
+		}
+		else {
+			String tempRemoved = text.get(index-1);
+			myText.remove(index-1);
+			showToUser(String.format(MSG_DELETED, fileName, tempRemoved));
+		}
+		
+		return myText;
+	}
+	
+	//displays the content of arraylist
+	//prints error msg if arraylist is empty
+	public boolean display(ArrayList<String> myText) {
+		boolean isEmpty = false;
+		
+		if(myText.size() == 0) {
+			showToUser(String.format(MSG_EMPTY, fileName));
+			isEmpty = true;
+		}
+		else {
+			for(int i = 0; i < myText.size(); i++) {
+				showToUser(String.format(MSG_DISPLAY, i+1, myText.get(i)));
+			}
+		}
+		return isEmpty;
+	}
+
+	//adds a user input string into the arraylist
+	public ArrayList<String> addText(ArrayList<String> myText, String temp) {
+		myText.add(temp);
+		showToUser(String.format(MSG_ADDED, fileName, temp));
+		return myText;
+	}
+	
 }
